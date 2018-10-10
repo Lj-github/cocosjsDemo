4ce8692cd74c19766f07f0c33fa1e468fbcc5c74
刘江 <1433727556@qq.com>
2018-10-10 17:54:58 +0800

    build

--- app/MakeFileListRES.py
+++ app/MakeFileListRES.py
@@ -11,15 +11,13 @@ import sys
 def GetFileList(dir, fileList):
     newDir = dir
     if os.path.isfile(dir):
-        fileList.append(dir.decode('utf-8'))
+        fileList.append(dir)
     elif os.path.isdir(dir):
         for s in os.listdir(dir):
             newDir = os.path.join(dir, s)
             GetFileList(newDir, fileList)
     return fileList
-def getFileStrLen(path):
-    if path :
-        return  path.decode('utf-8').__len__()
+
 
 try:
     #获取当前路径
@@ -31,9 +29,9 @@ try:
             newJson = oldJson
             newJson["jsList"] = []
             for jsfil in allJsList:
-                newJson["jsList"].append(jsfil[getFileStrLen(sys.path[0])+1:])
+                newJson["jsList"].append(jsfil[len(sys.path[0])+1:])
             with open("project.json", 'w') as f:
-                json.dump(newJson, f)
+                json.dump(newJson, f, sort_keys=True, indent=4, separators=(',', ':'))
 
 except NameError:
     pass
